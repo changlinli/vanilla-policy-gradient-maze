2a44dfb7fbddd7d58b10e7f372b3ef1d73568a54
Changlin Li <mail@changlinli.com>
2024-09-02 13:41:43 -0700

    First commit

---
+++ default.nix
@@ -0,0 +1,54 @@
+with import <nixpkgs> { };
+
+let
+  pythonPackages = python312Packages;
+in pkgs.mkShell rec {
+  name = "impurePythonEnv";
+  venvDir = "./.venv";
+  buildInputs = [
+
+    pkgs.stdenv.cc.cc.lib
+
+    git-crypt
+    # stdenv.cc.cc # jupyter lab needs
+
+    # pythonPackages.python
+    pythonPackages.ipykernel
+    pythonPackages.jupyterlab
+    pythonPackages.notebook
+    pythonPackages.pyzmq    # Adding pyzmq explicitly
+    pythonPackages.venvShellHook
+    pythonPackages.pip
+    pythonPackages.numpy
+    pythonPackages.pandas
+    pythonPackages.requests
+    pythonPackages.torch
+    pythonPackages.torchvision
+    pythonPackages.matplotlib
+
+    # sometimes you might need something additional like the following - you will get some useful error if it is looking for a binary in the environment.
+    taglib
+    openssl
+    git
+    libxml2
+    libxslt
+    libzip
+    zlib
+
+  ];
+
+  # Run this command, only after creating the virtual environment
+  postVenvCreation = ''
+    unset SOURCE_DATE_EPOCH
+    
+    python -m ipykernel install --user --name=myenv4 --display-name="myenv4"
+    pip install -r requirements.txt
+  '';
+
+  # Now we can execute any commands within the virtual environment.
+  # This is optional and can be left out to run pip manually.
+  postShellHook = ''
+    # allow pip to install wheels
+    unset SOURCE_DATE_EPOCH
+  '';
+}
