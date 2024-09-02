14c01adb27b38295b79e8c601cfe174e8eeb20d3
chisness <max.chiswick@gmail.com>
2024-09-02 17:38:51 -0400

    added rewards

--- main.py
+++ main.py
@@ -36,64 +36,69 @@ class Action(Enum):
 
 # %%
 
-def make_move(agent_state: tuple[int, int], action: Action) -> tuple[int, int]:
+def make_move(agent_state: tuple[int, int], action: Action) -> tuple[tuple[int, int], float]:
     agent_state_x, agent_state_y = agent_state
+    # small negative reward for each timestep, can end game at e.g. -100
+    # large positive reward for reaching end of game
     match action:
         case Action.UP:
             out_of_bounds = agent_state_y - 1 < 0
             if out_of_bounds:
-                return agent_state
+                new_state = agent_state
             else:
                 # Remember to reverse these coordinates because the first index
                 # will select the row and the second by column, so if you pass
                 # in (x, y) you should index first by y and then x
                 wall_in_way = fixed_maze[agent_state_y - 1][agent_state_x] == 1
                 if wall_in_way:
-                    return agent_state
-                else:
-                    return agent_state_x, agent_state_y - 1
+                    new_state = agent_state
+                new_state = agent_state_x, agent_state_y - 1
         case Action.DOWN:
-            out_of_bounds = agent_state_y + 1 < 0
+            out_of_bounds = agent_state_y + 1 > 4
             if out_of_bounds:
-                return agent_state
+                new_state = agent_state
             else:
                 # Remember to reverse these coordinates because the first index
                 # will select the row and the second by column, so if you pass
                 # in (x, y) you should index first by y and then x
                 wall_in_way = fixed_maze[agent_state_y + 1][agent_state_x] == 1
                 if wall_in_way:
-                    return agent_state
-                else:
-                    return agent_state_x, agent_state_y + 1
+                    new_state = agent_state
+                new_state = agent_state_x, agent_state_y + 1
         case Action.LEFT:
             out_of_bounds = agent_state_x - 1 < 0
             if out_of_bounds:
-                return agent_state
+                new_state = agent_state
             else:
                 # Remember to reverse these coordinates because the first index
                 # will select the row and the second by column, so if you pass
                 # in (x, y) you should index first by y and then x
                 wall_in_way = fixed_maze[agent_state_y][agent_state_x - 1] == 1
                 if wall_in_way:
-                    return agent_state
-                else:
-                    return agent_state_x - 1, agent_state_y
+                    new_state = agent_state
+                new_state = agent_state_x - 1, agent_state_y
         case Action.RIGHT:
-            out_of_bounds = agent_state_x + 1 < 0
+            out_of_bounds = agent_state_x + 1 > 4
             if out_of_bounds:
-                return agent_state
+                new_state = agent_state
             else:
                 # Remember to reverse these coordinates because the first index
                 # will select the row and the second by column, so if you pass
                 # in (x, y) you should index first by y and then x
                 wall_in_way = fixed_maze[agent_state_y][agent_state_x + 1] == 1
                 if wall_in_way:
-                    return agent_state
+                    new_state = agent_state
                 else:
-                    return agent_state_x + 1, agent_state_y
+                    new_state = agent_state_x + 1, agent_state_y
+
+    if new_state == (4,4):
+        return (new_state, 100)
+    else:
+        return (new_state, -1)
 
 print(f"{make_move((0, 1), Action.UP)=}")
 print(f"{make_move((0, 1), Action.DOWN)=}")
 print(f"{make_move((0, 1), Action.LEFT)=}")
 print(f"{make_move((0, 1), Action.RIGHT)=}")
 print(f"{make_move((0, 4), Action.RIGHT)=}")
+print(f"{make_move((4, 3), Action.DOWN)=}")
